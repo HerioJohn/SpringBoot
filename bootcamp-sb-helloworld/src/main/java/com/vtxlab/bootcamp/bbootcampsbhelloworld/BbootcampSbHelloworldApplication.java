2486d8f25886af6ef76925bc4e21f3ce85d32c1c
Wing.Yeung <heriojohnlifeofgame@gmail.com>
2024-01-29 10:37:48 +0800

    1-29-24_ProjectStart

---
+++ bootcamp-sb-helloworld/src/main/java/com/vtxlab/bootcamp/bbootcampsbhelloworld/BbootcampSbHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.vtxlab.bootcamp.bbootcampsbhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbootcampSbHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbootcampSbHelloworldApplication.class, args);
+	}
+
+}
---
+++ bootcamp-sb-helloworld/src/test/java/com/vtxlab/bootcamp/bbootcampsbhelloworld/BbootcampSbHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtxlab.bootcamp.bbootcampsbhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbootcampSbHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
